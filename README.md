a46364fb2625f48fa320014c3bdc17976508be3d
Arsia <Arsia.Chou@gmail.com>
2018-12-23 09:53:23 +0800

    improveOptions

--- README.md
+++ README.md
@@ -1,26 +1,23 @@
 ## chdns
-*学校校园网不修改DNS无法更新下载软件，于是写了这个小脚本方便自己修改DNS*
+*学校校园网不修改DNS无法更新下载软件，于是写了这个小程序方便自己修改DNS*
 
   -  语法参考：
         - chdns \[dns 1\] \[dns 2\] 
 
   - 一些选项：
-    - `-h` 帮助信息
-    - `-google` 将dns修改为`8.8.8.8`和`8.8.4.4`
-    - `-114dns` 将dns修改为`114.114.114.114`和`218.85.157.99`
-    - `-show` 查看当前dns信息
+    - `-h` `-help` 帮助信息
+    - `-g` `-google` 将dns修改为`8.8.8.8`和`8.8.4.4`
+    - `-114dns` 将dns修改为`114.114.114.114`和`114.114.115.115`
+    - `-s` `-show` 查看当前dns信息
 
 ## 注意
   - 修改的是NetworkManager创建的/etc/resolv.conf
   - 修改需要root权限，或者使用sudo， 不然会报块错误，不能写入
+  - `gcc chdns.c -o chdns` 后，将编译好的文件放入`/usr/bin`等目录，或者为该文件单独配置环境变量
 
 ## 示例
 
-<<<<<<< HEAD
 ```powershell
-=======
-```shell
->>>>>>> c9b30f5a2ed840508f9550fb76f28cf1e7882811
 [arsia]$chdns -show
  Now file content(/etc/resolv.conf):
 	#Change by chdns
@@ -34,8 +31,20 @@
 	nameserver 8.8.4.4
 	
 
-  chdns: Do you want to change?(y or n):  
+  chdns: Do you want to change?(y or n): 
   
   chdns: Message( nameserver 114.114.114.114 nameserver 218.85.157.99)
+  chdns: Succeccful!
+  
+[arsia]$sudo chdns 114.114.114.114 114.114.115.115
+  Now file content(/etc/resolv.conf):
+	#Change by chdns
+	nameserver 8.8.8.8
+	nameserver 8.8.4.4
+	
+
+  chdns: Do you want to change?(y or n): y
+  
+  chdns: Message( nameserver 114.114.114.114 nameserver 114.114.115.115)
   chdns: Succeccful!
 ```
--- chdns.c
+++ chdns.c
@@ -14,9 +14,9 @@ char *Usage = "\n  chdns: A script to change dns. (NetworkManager) [need sudo]\
                \n         Change /etc/resolv.conf\
                         \n\n\t chdns [dns1] [dns2]\
                         \n\n\tOptions: \
-                        \n\n\t -google (8.8.8.8 & 8.8.4.4)\
-                        \n\n\t -114dns (114.114.114.114 & 218.85.157.99)\
-                        \n\n\t -show print dns file content.(/etc/resolv.conf)\n\n";
+                        \n\n\t -g -google (8.8.8.8 & 8.8.4.4)\
+                        \n\n\t -114dns (114.114.114.114 & 114.114.115.115)\
+                        \n\n\t -s -show print dns file content.(/etc/resolv.conf)\n\n";
 
 /*
 *   dns file path.
@@ -26,18 +26,25 @@ char *dnsPath = "/etc/resolv.conf";
 /*
 *   '-h', -google' and '-114dns' pattern
 */
-char *help = "-h", *google = "-google", *dns114 = "-114dns", *show = "-show";
+char *help = "-help", *google = "-google", *dns114 = "-114dns", *show = "-show";
+char *h = "-h",       *g = "-g",                                *s = "-s";
+
+/*
+*   dns
+*/
+char *g_dns_1 = "8.8.8.8", *g_dns_2 = "8.8.4.4",
+        *dns_114_1 = "114.114.114.114", *dns_114_2 = "114.114.115.115";
 
 /*
 *   When there is only one option
 */
 void oneOption(char *option){
-    if (0 == strcmp(help, option)){
+    if (0 == strcmp(help, option)|| 0 == strcmp (h, option)){
         printf ("%s",Usage);
-    }else if (0 == strcmp(google, option)){
+    }else if (0 == strcmp(google, option) || 0 == strcmp (g, option)){
         readFile();
         if (1 == getAnswer("  chdns: Do you want to change?(y or n):  ")){
-            writeDns("8.8.8.8", "8.8.4.4");
+            writeDns(g_dns_1, g_dns_2);
         }else{
             printf ("  Refuse to change.\n");
             return;
@@ -45,12 +52,12 @@ void oneOption(char *option){
     }else if (0 == strcmp(dns114, option)){
         readFile();
         if (1 == getAnswer("  chdns: Do you want to change?(y or n):  ")){
-            writeDns("114.114.114.114", "218.85.157.99");
+            writeDns(dns_114_1, dns_114_2);
         }else{
             printf ("  Refuse to change.\n");
             return;
         }
-    }else if (0 == strcmp (show, option)){
+    }else if (0 == strcmp (show, option) || 0 == strcmp (s, option)){
         readFile();
     }else {
         printf ("  chdns: There is no such option.('-h' for help)\n\n");
@@ -63,15 +70,15 @@ void oneOption(char *option){
 int writeDns(char *dns1, char *dns2){
     char *ns = "nameserver = ";
     FILE *resolv = fopen (dnsPath,"w+");
-    printf ("\n  chdns: Message( nameserver %s nameserver %s)\n\n", dns1, dns2);
-    int state = fprintf (resolv, "#Change by chdns\nnameserver %s\nnameserver %s\n\n", dns1, dns2);
+    printf ("\n  chdns: Message( nameserver %s nameserver %s)\n", dns1, dns2);
+    int state = fprintf (resolv, "#Change by chdns\nnameserver %s\nnameserver %s\n", dns1, dns2);
     fclose (resolv);
 
     if (state < 0){
-        printf ("  chdns: fail to write.\n\n");
+        printf ("  chdns: fail to write.\n");
         return 0;
     }else{
-        printf ("  chdns: Succeccful!\n\n");
+        printf ("  chdns: Succeccful!\n");
         return 1;
     }
 }
@@ -82,7 +89,7 @@ int writeDns(char *dns1, char *dns2){
 void readFile(){
     FILE *resolv = fopen (dnsPath, "r");
     char c = ' ';
-    printf ("\n Now file content(%s):\n\t", dnsPath);
+    printf ("\n  chdns: Now file content(%s):\n\t", dnsPath);
     while ((c = fgetc(resolv)) != EOF){
         putchar (c);
         if (c == '\n'){
