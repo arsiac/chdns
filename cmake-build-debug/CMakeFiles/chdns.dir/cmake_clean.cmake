5501be30447b421f88e5be37f9ecb53412eb6298
Arsia Chou <arsia.chou@gmail.com>
2019-02-20 00:21:33 +0800

    clone

---
+++ cmake-build-debug/CMakeFiles/chdns.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chdns.dir/chdns.c.o"
+  "chdns.pdb"
+  "chdns"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chdns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
