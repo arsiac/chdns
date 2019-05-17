4a9a79809e01fd250d4599cd6d416bb689262e8f
Arsia Chou <Arsia.Chou@gmail.com>
2019-05-17 19:40:28 +0800

    test permission

--- chdns.c
+++ chdns.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void oneOption(char *option);
 int writeDns(char *dns1, char *dns2);
@@ -42,16 +43,16 @@ void oneOption(char *option){
     if (0 == strcmp(help, option)|| 0 == strcmp (h, option)){
         printf ("%s",Usage);
     }else if (0 == strcmp(google, option) || 0 == strcmp (g, option)){
-        readFile();
-        if (1 == getAnswer("chdns: Do you want to change?(y or n):  ")){
+//        readFile();
+        if (1 == getAnswer("Do you want to change?(y or n):  ")){
             writeDns(g_dns_1, g_dns_2);
         }else{
             printf ("Refuse to change.\n");
             return;
         }
     }else if (0 == strcmp(dns114, option)){
-        readFile();
-        if (1 == getAnswer("chdns: Do you want to change?(y or n):  ")){
+//        readFile();
+        if (1 == getAnswer("Do you want to change?(y or n):  ")){
             writeDns(dns_114_1, dns_114_2);
         }else{
             printf ("Refuse to change.\n");
@@ -60,7 +61,7 @@ void oneOption(char *option){
     }else if (0 == strcmp (show, option) || 0 == strcmp (s, option)){
         readFile();
     }else {
-        printf ("chdns: There is no such option.('-h' for help)\n");
+        printf ("There is no such option.('-h' for help)\n");
     }
 }
 
@@ -69,17 +70,22 @@ void oneOption(char *option){
 */
 int writeDns(char *dns1, char *dns2){
     char *ns = "nameserver = ";
-    FILE *resolv = fopen (dnsPath,"w+");
-    printf ("\n  chdns: Message( nameserver %s nameserver %s)\n", dns1, dns2);
-    int state = fprintf (resolv, "#Change by chdns\nnameserver %s\nnameserver %s\n", dns1, dns2);
-    fclose (resolv);
+    int write_p = access(dnsPath, W_OK);
 
-    if (state < 0){
-        printf ("chdns: fail to write.\n");
-        return 0;
-    }else{
-        printf ("chdns: Succeccful!\n");
-        return 1;
+    if (write_p == 0){
+        FILE *resolv = fopen (dnsPath,"w+");
+        int state = fprintf (resolv, "#Change by chdns\nnameserver %s\nnameserver %s\n", dns1, dns2);
+        fclose (resolv);
+
+        if (state < 0){
+            printf ("fail to write.\n");
+            return 0;
+        }else{
+            printf ("Message( nameserver %s nameserver %s)\nSucceccful!\n", dns1, dns2);
+            return 1;
+        }
+    } else {
+        printf("Permission denied.\n");
     }
 }
 
@@ -93,7 +99,7 @@ void readFile(){
     while ((c = fgetc(resolv)) != EOF){
         putchar (c);
         if (c == '\n'){
-            printf ("\t");
+            printf ("\t*");
         }
     }
     putchar('\n');
@@ -135,8 +141,8 @@ int main (int argc, char *argv[]){
                 break;
 
             case 3:
-                readFile();
-                if (1 == getAnswer("chdns: Do you want to change?(y or n):  ")){
+//                readFile();
+                if (1 == getAnswer("Do you want to change?(y or n):  ")){
                     writeDns(argv[1], argv[2]);
                 }else{
                     printf ("Refuse to change.\n");
@@ -144,7 +150,7 @@ int main (int argc, char *argv[]){
                 break;
 
             default:
-                printf ("chdns: \n\t Too many variables: %d\n",argc-1);
+                printf ("Too many variables: %d\n",argc-1);
 
         }
     return 0;
